405b9c4f3f0175a1bb850e43a9cfd7f71e330f02
calebmortensen <caleb.mortensen@live.com>
2024-09-29 10:08:23 -0700

    Intent Data passed to Activity

--- app/src/main/java/com/example/counterapp/SecondActivity.java
+++ app/src/main/java/com/example/counterapp/SecondActivity.java
@@ -1,6 +1,11 @@
 package com.example.counterapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,38 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
 
+    EditText enter_name;
+    Button btn_wish;
+    TextView welcomeNumber;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //Initialize Widgets
+        welcomeNumber = findViewById(R.id.welcomeNumber);
+        enter_name = findViewById(R.id.enter_name);
+        btn_wish = findViewById(R.id.btn_wish);
+
+        //Functions -> Pass DATA between Activities
+
+        btn_wish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = enter_name.getText().toString();
+                //Explicit Intent -> Will be sending data to Third Activity
+                Intent i = new Intent(
+                        getApplicationContext(),
+                        ThirdActivity.class
+                );
+                //Passing name to third activity
+                i.putExtra("name", userName);
+                startActivity(i);
+            }
         });
+
+    };
     }
-}
---
+++ app/src/main/java/com/example/counterapp/ThirdActivity.java
@@ -0,0 +1,75 @@
+package com.example.counterapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class ThirdActivity extends AppCompatActivity {
+
+
+    TextView yourNumber, numberOutput;
+    Button share_btn;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+
+        yourNumber = findViewById(R.id.yourNumber);
+        numberOutput = findViewById(R.id.numberOutput);
+        share_btn = findViewById(R.id.share_btn);
+
+        //How to receive (GET) data from Second Activity from putExtra Method
+        Intent i = getIntent();
+        String userName = i.getStringExtra("name");
+
+        int random_num = generateRandomNumber();
+        numberOutput.setText(""+random_num);
+
+
+        //SHARING DATA to VARIOUS APPS OPTION
+
+        share_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareData(userName, random_num);
+            }
+        });
+
+        };
+    //Generate Random Numbers
+
+    public int generateRandomNumber(){
+        Random random = new Random();
+        int upper_limit = 1000;
+        int randomNumberGenerated = random.nextInt(upper_limit);
+        return randomNumberGenerated;
+    }
+
+    public void shareData(String username, int randomNum ){
+        //Implicit Intent
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        //Additional Info
+        i.putExtra(Intent.EXTRA_SUBJECT,username + "Got lucky today!");
+        i.putExtra(Intent.EXTRA_TEXT, "Your lucky number is: " +randomNum);
+        startActivity(Intent.createChooser(i, "Choose a Platform"));
+
+    }
+    }
+
+
+
